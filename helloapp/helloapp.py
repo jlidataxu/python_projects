7f6b0f120b892233627306d54c544aa1eceaf79d
James Li <jli@dataxu.com>
2015-12-24 18:56:20 -0500

    0.1 create helloapp folder and add debug mode into the code

--- helloapp/helloapp.py
+++ helloapp/helloapp.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello Beautiful World'
+	return 'Hello Beautiful World!!' 
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
