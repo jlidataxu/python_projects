7f8dbd7b64cea6ff996a879f64bc88a14d484a7b
James Li <zliyclj1@gmail.com>
2016-01-05 14:54:55 +0000

    flaskblog0.3 1) base template and bootstrap 2) use WTForms

--- flask_blog/author/views.py
+++ flask_blog/author/views.py
@@ -1,5 +1,12 @@
+from flask import render_template
 from flask_blog import app
+from author.form import RegisterForm
 
 @app.route('/login')
 def login():
     return "Hello, User!"
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    return render_template('author/register.html', form=form)
