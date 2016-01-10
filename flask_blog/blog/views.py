f874d9c61479ea3b6d95881f6543bd8f5a0e91ac
James Li <zliyclj1@gmail.com>
2016-01-10 16:36:31 +0000

    flaskblog0.7 Blog creation database

--- flask_blog/blog/views.py
+++ flask_blog/blog/views.py
@@ -1,5 +1,5 @@
 from flask_blog import app
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, request
 from blog.form import SetupForm
 from flask_blog import db
 from author.models import Author
@@ -17,7 +17,35 @@ def admin():
         return redirect(url_for('setup'))
     return render_template('blog/admin.html')
     
-@app.route('/setup')
+@app.route('/setup', methods=('GET', 'POST'))
 def setup():
+    error = ""
     form = SetupForm()
-    return render_template('blog/setup.html', form=form)
+    if request.method == 'POST' and form.validate():
+        author = Author(
+            form.fullname.data,
+            form.email.data,
+            form.username.data,
+            form.password.data,
+            True
+        )
+        db.session.add(author)
+        db.session.flush()
+        if author.id:
+            blog = Blog(
+                form.name.data,
+                author.id
+            )
+            db.session.add(blog)
+            db.session.flush()
+        else:
+            db.session.rollback()
+            error = "Error creating user"
+        if author.id and blog.id:
+            db.session.commit()
+            flash("Blog created")
+            return redirect(url_for("admin"))
+        else:
+            db.session.rollback()
+            error = "Error creating blog"
+    return render_template('blog/setup.html', form=form, error=error)
